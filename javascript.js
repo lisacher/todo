2c1903d2c6fec3fe1665455d201fd8730d03bd18
lisacher <78444589+lisacher@users.noreply.github.com>
2021-03-12 01:56:04 +0800

    新增

---
+++ javascript.js
@@ -0,0 +1,70 @@
+// 初始變數 抓取ＤＯＭ
+const list = document.querySelector('#my-todo')
+const addBtn = document.querySelector('#addBtn')
+const input = document.querySelector('#newTodo')
+const done = document.querySelector('#my-done')
+
+// 原始就有的ＴＯＤＯ資料
+const todos = ['上健身房', '每日讀好書', '喝水2500ml', '跑步', '學習']
+for (let todo of todos) {
+  addItem(todo)
+}
+
+
+// 函式
+function addItem (text) {
+//新增Todo的字列
+  const newItem = document.createElement('li')
+  newItem.innerHTML = `
+    <label for="todo">${text}</label>
+    <i class="delete fa fa-trash"></i>
+  `
+  list.appendChild(newItem)
+}
+
+//檢查字元有無效
+function checkItem (){
+   let inputValue = input.value
+   if(inputValue.trim().length === 0){
+    alert('請輸入項目')
+  }else{
+    addItem(inputValue)
+  }
+}
+
+// //設置監聽器(增加項目)
+addBtn.addEventListener('click', function(){
+  checkItem()
+})
+// 用Enter發送
+input.addEventListener('keypress', function(){
+  if (event.keyCode == 13){
+    checkItem()
+  }
+})
+
+// //監聽刪除父元素
+list.addEventListener('click',function(event){
+  let target = event.target
+  if (target.classList.contains('delete')){
+    let parentElement = target.parentElement
+    parentElement.remove()
+  }
+  else if ( target.tagName === 'LABEL'){
+    target.classList.toggle('checked')
+    done.append(target.parentElement)
+  }
+})
+
+done.addEventListener('click',function(){
+  let target = event.target
+  if (target.classList.contains('delete')){
+    const parentElement = target.parentElement
+    parentElement.remove()
+  }
+  else if ( target.tagName === 'LABEL'){
+    target.classList.toggle('checked')
+    list.append(target.parentElement)
+  }
+  
+})
